2a126a24ff39a1e8fe326d26e67d7dd5054bb4ae
StackDoubleFlow <ojaslandge@gmail.com>
2021-06-17 08:17:12 -0500

    Fix file name in copy script

--- copy.sh
+++ copy.sh
@@ -2,7 +2,7 @@
 
 cargo ndk build
 
-adb push target/aarch64-linux-android/debug/librust_test2.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/
+adb push target/aarch64-linux-android/debug/libburn_marks.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/
 adb shell am force-stop com.beatgames.beatsaber
 adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
 
